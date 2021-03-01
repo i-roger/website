a80919d3323aa98f129e6ae69f91af81ef861298
i-roger <58693188+i-roger@users.noreply.github.com>
2021-03-01 09:54:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/* Meu primeiro script
+ Criado por : Roger */
+
+// Função criada para ser chamada no EVENT
+//function mensagem(){
+//alert('Hello my friend.');
+//}
+
+// criação de variável para ser chamado no ALERT
+//var mensagem = "Welcome to my website"
+//alert(mensagem);
+
+// Usado simplesmente para carregar mensagem no start do site
+//alert("Seja Bem Vindo!");
